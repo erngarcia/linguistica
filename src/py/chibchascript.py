9fafe546975bbddb6066d347b2e484b555c371f6
David Jiménez <djimenez81@yahoo.com>
2020-10-30 08:24:01 -0600

    Created script

--- src/py/chibchascript.py
+++ src/py/chibchascript.py
@@ -2,4 +2,4 @@ from pandas import read_excel
 
 df_vocales = read_excel()
 
-df_vocales.to_json(r'C:\Users\luses\Documents\U\UCR\Asistencia\Metricas\linguistica\vocales.json')
+# df_vocales.to_json(r'C:\Users\luses\Documents\U\UCR\Asistencia\Metricas\linguistica\vocales.json')
